fe2663169136218f5ba61683a4c7ac41a37057e0
IZAY01 <IZAY01@qq.com>
2020-12-18 17:11:27 +0800

    beta version v0.0.2

--- NekoEar2Au.rs
+++ NekoEar2Au.rs
@@ -4,42 +4,37 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::mem::transmute;
 
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() <= 1 {
-        println!("Usage:");
+        println!("Usage: ./NekoEar2Au source_file1 source_file2 ...");
         return;
     }
 
     for next_file_path in &args[1..] {
         conversion(&next_file_path);
     }
-
-    println!("Done!");
 }
+
 
 fn conversion(origin_file_path: &String) {
     let origin_path = Path::new(origin_file_path);
     let origin_display = origin_path.display();
     let mut origin_fd = match File::open(origin_file_path) {
-        Err(why) => panic!("could not open {}: {}", origin_display, why),
+        Err(why) => panic!("Couldn't open {}: {}", origin_display, why),
         Ok(fd) => fd,
     };
 
     let mut buffer = Vec::new();
-    match origin_fd.read_to_end(& mut buffer) {
-        Err(why) => panic!("could not read {}: {}", origin_display, why), 
-        Ok(_) => (),
-    };
+    origin_fd.read_to_end(& mut buffer).expect("Read data failed!");
 
     let (au_start_addr, au_file_size) = unsafe {
         ( transmute::<[u8; 4], u32>([buffer[20], buffer[21], buffer[22], buffer[23]]),
         transmute::<[u8; 4], u32>([buffer[40], buffer[41], buffer[42], buffer[43]]) )
     };
     
-    println!("{}, {}", au_start_addr, au_file_size);
-
     let head_index = au_start_addr as usize;
     let mut target_file_path = String::from(origin_file_path);
 
@@ -48,7 +43,7 @@ fn conversion(origin_file_path: &String) {
     && buffer[head_index+2] == 0x33)
     || (buffer[head_index] == 0xff
     && buffer[head_index+1] == 0xfb){
-        println!("mp3 file!");
+        print!("mp3 file!  ");
         target_file_path += ".mp3";
     }
 
@@ -63,12 +58,12 @@ fn conversion(origin_file_path: &String) {
     && buffer[head_index+1] == 0x34
     && buffer[head_index+2] == 0x41
     && buffer[head_index+3] == 0x21) {
-        println!("m4a file!");
+        print!("m4a file!  ");
         target_file_path += ".m4a";
     }
 
     else {
-        println!("other file format...");
+        print!("other file format...  ");
         target_file_path += ".audio";
     }
 
@@ -79,5 +74,7 @@ fn conversion(origin_file_path: &String) {
         Ok(fd) => fd,
     };
 
-    target_fd.write_all(&buffer[au_start_addr as usize .. (au_start_addr+au_file_size) as usize]).expect("write failed!");
+    target_fd.write_all(&buffer[au_start_addr as usize .. (au_start_addr+au_file_size) as usize]).expect("Write data failed!");
+    
+    println!("{}", target_display);
 }    
